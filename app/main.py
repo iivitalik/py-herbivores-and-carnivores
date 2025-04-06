f3e66051de4ceb1e1f8d22215faf8bd0a7b58218
Vitalik <lg.mobile0808@gmail.com>
2025-04-06 16:21:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,35 +1,42 @@
 class Animal:
     alive = []
+
     def __init__(
             self,
-            health: int=100,
-            name: str=None,
-            hidden: bool=False
+            name: str,
+            health: int = 100,
+            hidden: bool = False
     ) -> None:
-        self.health = health
         self.name = name
+        self.health = health
         self.hidden = hidden
         self.__class__.alive.append(self)
 
-    def take_damage(self, amount: int):
-        self.health -= amount
         if self.health <= 0:
             self.__class__.alive.remove(self)
 
+    def __str__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+    @classmethod
+    def __repr__(cls) -> str:
+        return f"[{', '.join(str(animal) for animal in cls.alive)}]"
+
 
 class Herbivore(Animal):
-    def hide(self, name: str) -> None:
-        self.name = name
+    def hide(self) -> None:
         self.hidden = not self.hidden
+        print(f"{self.name} is now {'hiding' if self.hidden else 'visible'}.")
 
 
 class Carnivore(Animal):
-    def bite(self, herb : Herbivore) -> None:
-        if not herb.hidden and isinstance(herb, Herbivore):
-            herb.health -= 50
-        if herb.health <= 0:
-            Animal.alive.remove(herb)
-
-
-
-
+    def bite(self, herb: Herbivore) -> None:
+        if isinstance(herb, Herbivore):
+            if not herb.hidden:
+                herb.health -= 50
+            else:
+                print(f"{herb.name} is hiding, can't bite.")
+        else:
+            print(f"{self.name} cannot bite another carnivore.")
